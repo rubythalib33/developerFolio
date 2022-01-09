2873a5d6eff92c46305b18c2e664a89bf99ed3dd
rubythalib33 <abdullah3333abdul@gmail.com>
2022-01-09 08:51:48 +0700

    web portofolio

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,12 +5,8 @@ import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
-import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
 import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
 import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 
@@ -24,11 +20,7 @@ export default class Main extends Component {
         <StackProgress />
         <Projects />
         <StartupProject />
-        <Achievement />
-        <Blogs />
-        <Talks />
         <Twitter />
-        <Podcast />
         <Contact />
         <Footer />
         <Top />
